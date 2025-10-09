d395d37091f81579bd98d1ec54444d7bca0a45af
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-09 05:54:39 +0100

    fix: restructure base layout, meta and styles

--- src/pages/seo.ts
+++ src/pages/seo.ts
@@ -2,6 +2,7 @@ export const website = {
   name: "Oghenetefa Okotete",
   description: `This is the personal website of Oghenetefa Okotete, where I share my projects, blog posts, and more.`,
   creator: "oghenetefa@gmail.com",
+  email: "oghenetefa@gmail.com",
 } as const;
 
 export const socials = [
@@ -18,6 +19,21 @@ export const socials = [
     href: "https://www.linkedin.com/in/markhorn-dev",
   },
 ] as const;
+
+export const socialLinks = {
+  twitter: {
+    title: "twitter-x",
+    href: "https://twitter.com/everustruly",
+  },
+  github: {
+    title: "github",
+    href: "https://github.com/everustruly",
+  },
+  linkedin: {
+    title: "linkedin",
+    href: "https://linkedin.com/in/everustruly",
+  },
+} as const;
 
 export type Metadata = {
   title: string;
