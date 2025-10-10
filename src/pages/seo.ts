8a2249471401782c0244787eb6f681781f62c0ec
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-10 13:16:10 +0100

    fix: update and remove broken links

--- src/pages/seo.ts
+++ src/pages/seo.ts
@@ -1,37 +1,21 @@
 export const website = {
   name: "Oghenetefa Okotete",
   description: `This is the personal website of Oghenetefa Okotete, where I share my projects, blog posts, and more.`,
-  creator: "oghenetefa@gmail.com",
   email: "oghenetefa@gmail.com",
 } as const;
-
-export const socials = [
-  {
-    title: "twitter-x",
-    href: "https://twitter.com/markhorn_dev",
-  },
-  {
-    title: "github",
-    href: "https://github.com/markhorn-dev",
-  },
-  {
-    title: "linkedin",
-    href: "https://www.linkedin.com/in/markhorn-dev",
-  },
-] as const;
 
 export const socialLinks = {
   twitter: {
     title: "twitter-x",
-    href: "https://twitter.com/everustruly",
+    href: "https://twitter.com/everurstruly",
   },
   github: {
     title: "github",
-    href: "https://github.com/everustruly",
+    href: "https://github.com/everurstruly",
   },
   linkedin: {
     title: "linkedin",
-    href: "https://linkedin.com/in/everustruly",
+    href: "https://linkedin.com/in/everurstruly",
   },
 } as const;
 
