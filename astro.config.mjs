386d925bccb68aa89cd14a7f7d3b2206cfce61e4
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-16 23:39:09 +0100

    feat remove react based integrations and files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
 export default defineConfig({
   site: "https://everurstruly.netlify.app",
-  integrations: [react(), mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
